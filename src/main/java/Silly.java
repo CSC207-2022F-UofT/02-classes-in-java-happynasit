2062215f42d5b84d6cfe698e40cd6ddcc245f5c7
happynasit <happynasit@gmail.com>
2022-10-08 18:13:33 -0400

    Completed ex2- CSC207

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 /**
  * This file contains a few exercises to familiarize you with specific
  * class features in Java.
- *
+
  * You should read this file from top-to-bottom. Any tasks you are to complete
  * are labelled with TODO
- *
+
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
@@ -16,11 +16,11 @@ public class Silly implements Comparable<Silly>{
      * 1. Java has variables known as "static" variables.
      * These are variables that exist in every instance of a class,
      * and which have the same value across all instances.
-     *
+
      * Below we have created a static variable (signified by the keyword
      * static) named my_static. The countStatic() method makes use of this
      * static variable.
-     *
+
      * (Relevant reading: 2.1. Static variables)
      */
     public static int my_static = 0;
@@ -45,10 +45,10 @@ public class Silly implements Comparable<Silly>{
      * 2. Java allows for us to overload methods: this means we can have
      *    multiple methods of the same name, but which take different
      *    parameters (both in terms of numbers and type).
-     *
+
      *    Below we have created two different constructors and have left
      *    a third for you to write.
-     *
+
      *    (Relevant reading: 2.2. Overloading)
      */
 
@@ -80,7 +80,9 @@ public class Silly implements Comparable<Silly>{
      *       Make sure you document this method!
      */
 
-
+    public Silly(String first, String second){
+        this.name = first + second;
+    }
 
 
 
@@ -116,7 +118,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,7 +135,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +163,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return other.name.equals(this.name);
     }
 
     /**
@@ -171,11 +174,11 @@ public class Silly implements Comparable<Silly>{
      *    ii) override the compareTo method. This takes another object
      *        (of the same type if we specify the classname when we write
      *        'implements Comparable'), and returns an integer.
-     *
+
      *    (Relevant reading: 2.5. Comparing objects)
-     *
+
      * Compares this object with other for order.
-     *
+
      * Returns a negative integer, zero, or a positive integer as this
      * object is less than, equal to, or greater than the specified object.
      *
@@ -194,6 +197,15 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+        if (this.name.length() > other.name.length()){
+            return 1;
+        }
+        else if (this.name.length() < other.name.length()){
+            return -1;
+        }
+        else{
+            return 0;
+        }
     }
 
     /*
